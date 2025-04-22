a451ceb86761ba24d7e1fa5b3012ba0c28f67eca
IzanGual <iesvda.izamar@gmail.com>
2025-04-22 22:09:34 +0200

    V0.8(Añadida la funcionmalidad de comprobar la validec del profesor cuando intenta acceder a su aula en validatetoken)

--- auth/validate-token.php
+++ auth/validate-token.php
@@ -11,6 +11,9 @@ header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Methods: POST, OPTIONS");
 header("Access-Control-Allow-Headers: Content-Type, Authorization");
 
+require 'jwtHelper.php';
+require '../db/dbAulas.php';   
+
 // Manejo de preflight request (CORS)
 if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
     http_response_code(204);
@@ -20,46 +23,147 @@ if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
 $dotenv = Dotenv\Dotenv::createImmutable(dirname(__DIR__));
 $dotenv->load(); 
 
-$headers = getallheaders();
-$jwt = str_replace("Bearer ", "", $headers['Authorization'] ?? '');
+if (isset($_GET['accion'])) {
+    $accion = $_GET['accion'];
 
-// Verificar si hay un token en la solicitud
-if (!$jwt) {
-    response(200, [
-        "success" => false,
-        "error" => "missingToken"
-    ]);
+    switch ($accion) {
+        case 'validateUserToken':
+            validateUserToken();
+            break;
+
+        case 'validateTeacherToken':
+            validateTeacherToken();
+            break;
+
+        case 'validateStudentToken':
+            echo "validateStudentToken";
+            break;
+
+        default:
+            response(200, [
+                "success" => false,
+                "error" => "nonAccionGiven"
+            ]);
+            break;
+    }
+
+    exit;
 }
 
-try {
-    // Decodificar el JWT con la clave secreta
-    $decoded = JWT::decode($jwt, new Key($_ENV['JWT_SECRET'], 'HS256'));
+/**
+ * Valida el token del usuario
+ */
+function validateUserToken() {
+    $headers = getallheaders();
+    $jwt = str_replace("Bearer ", "", $headers['Authorization'] ?? '');
 
-    response(200, [
-        "success" => true,
-        "message" => "validToken"
-    ]);
+    // Verificar si hay un token en la solicitud
+    if (!$jwt) {
+        response(200, [
+            "success" => false,
+            "error" => "missingToken"
+        ]);
+    }
 
-} catch (ExpiredException $e) {
-    response(200, [
-        "success" => false,
-        "error" => "tokenExpired"
-    ]);
-} catch (BeforeValidException $e) {
-    response(200, [
-        "success" => false,
-        "error" => "tokenNotYetValid"
-    ]);
-} catch (SignatureInvalidException $e) {
-    response(200, [
-        "success" => false,
-        "error" => "invalidTokenSignature"
-    ]);
-} catch (Exception $e) {
-    response(200, [
-        "success" => false,
-        "error" => "invalidToken"
-    ]);
+    try {
+        // Decodificar el JWT con la clave secreta
+        $decoded = JWT::decode($jwt, new Key($_ENV['JWT_SECRET'], 'HS256'));
+
+        response(200, [
+            "success" => true,
+            "message" => "validToken"
+        ]);
+
+    } catch (ExpiredException $e) {
+        response(200, [
+            "success" => false,
+            "error" => "tokenExpired"
+        ]);
+    } catch (BeforeValidException $e) {
+        response(200, [
+            "success" => false,
+            "error" => "tokenNotYetValid"
+        ]);
+    } catch (SignatureInvalidException $e) {
+        response(200, [
+            "success" => false,
+            "error" => "invalidTokenSignature"
+        ]);
+    } catch (Exception $e) {
+        response(200, [
+            "success" => false,
+            "error" => "invalidToken"
+        ]);
+    }
+}
+
+/**
+ * Valida el token del usuario
+ */
+function validateTeacherToken() {
+    $headers = getallheaders();
+    $jwt = str_replace("Bearer ", "", $headers['Authorization'] ?? '');
+
+    // Verificar si hay un token en la solicitud
+    if (!$jwt) {
+        response(200, [
+            "success" => false,
+            "error" => "missingToken"
+        ]);
+    }
+
+    try {
+        // Decodificar el JWT con la clave secreta
+        $decoded = JWT::decode($jwt, new Key($_ENV['JWT_SECRET'], 'HS256'));
+
+        // Verificar que el role sea 'teacher'
+        if (isset($decoded->role) && ($decoded->role === 'teacher')) {
+            $teacherId = getUserIdFromToken();
+            $db = new dbAulas();
+
+            $isTheTeacher = $db->isTheTeacher($_GET['aulaId'], $teacherId);
+
+            if($isTheTeacher){
+                response(200, [
+                    "success" => true,
+                    "message" => "validTeacherToken"
+                ]);
+            }else{
+                response(200, [
+                    "success" => false,
+                    "error" => "notTheTeacher"
+                ]);
+            }
+
+            
+        } else {
+            response(200, [
+                "success" => false,
+                "error" => "invalidRole"
+            ]);
+        }
+
+    } catch (ExpiredException $e) {
+        response(200, [
+            "success" => false,
+            "error" => "tokenExpired"
+        ]);
+    } catch (BeforeValidException $e) {
+        response(200, [
+            "success" => false,
+            "error" => "tokenNotYetValid"
+        ]);
+    } catch (SignatureInvalidException $e) {
+        response(200, [
+            "success" => false,
+            "error" => "invalidTokenSignature"
+        ]);
+    } catch (Exception $e) {
+        response(200, [
+            "success" => false,
+            "error" => "invalidToken"
+        ]);
+    }
 }
 
 /**
